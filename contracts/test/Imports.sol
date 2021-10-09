b06404c5fe0710eab5fb9595867626f053876c7a
William Griffiths <willjgriff@gmail.com>
2021-10-09 15:03:24 +0100

    Replaced AragonVault with the FundsManager interface.

--- contracts/test/Imports.sol
+++ contracts/test/Imports.sol
@@ -4,6 +4,6 @@ import "@aragon/os/contracts/acl/ACL.sol";
 import "@aragon/os/contracts/kernel/Kernel.sol";
 import "@aragon/os/contracts/factory/DAOFactory.sol";
 import "@aragon/os/contracts/factory/EVMScriptRegistryFactory.sol";
+import "@1hive/funds-manager/contracts/AragonVaultFundsManager.sol";
 
-contract Imports {
-}
+contract Imports {}
